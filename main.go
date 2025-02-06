ec4e1057a45e9bbd0be7151a80e6725f27571ac9
Pajar Wijaya Putra <pajar.putra.aux@flip.id>
2025-02-06 18:06:49 +0700

    add middleware and routing

--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 	"log"
 	"net/http"
 	"product-service/db/mysql"
+	productHandler "product-service/handler/product"
+	"product-service/middleware"
+	productRepo "product-service/repository/product"
+	productUsecase "product-service/usecase/product"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	mysql.Connect()
+
 	router := mux.NewRouter()
+	productRepository := productRepo.NewProductRepository(mysql.MySQL)
+	productUsecase := productUsecase.NewProductUsecase(productRepository)
+	productHandler := productHandler.NewProductHandler(productUsecase)
+	router.Handle("/product/register", middleware.JWTMiddleware(http.HandlerFunc(productHandler.Register))).Methods(http.MethodPost)
+	router.Handle("/product", middleware.JWTMiddleware(http.HandlerFunc(productHandler.Register))).Methods(http.MethodGet)
 
 	fmt.Println("server is running")
 	err := http.ListenAndServe(":8001", router)
